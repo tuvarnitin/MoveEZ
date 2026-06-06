2f3d8f43a377af6e9ae3c4252a2065354900c389
tuvarnitin <nitintuvar2003@gmail.com>
2026-06-06 12:03:24 +0530

    Initialized redux and created authSlice and store

---
+++ client/src/redux/features/authSlice.js
@@ -0,0 +1,36 @@
+import {createSlice} from "@reduxjs/toolkit"
+
+const initialState = {
+    user:null,
+    isAuthenticated:false,
+    isAuthModalOpen:false,
+    currState:"login"
+}
+
+const authSlice = createSlice({
+    name:"auth",
+    initialState,
+    reducers:{
+        loginSuccess:(state,action)=>{
+            state.user = action.payload.user
+            state.isAuthenticated = true
+            localStorage.setItem("name",action.payload.user.name)
+        },
+        onLogout:(state,action)=>{
+            state.user = null
+            state.isAuthenticated = false
+        },
+        openAuthModal:(state)=>{
+            state.isAuthModalOpen = true
+        },
+        closeAuthModal:(state)=>{
+            state.isAuthModalOpen = false
+        },
+        setCurrState:(state,action)=>{
+            state.currState = action.payload.state
+        }
+    }
+})
+
+export const { loginSuccess, onLogout, openAuthModal, closeAuthModal, setCurrState } = authSlice.actions
+export default authSlice.reducer
