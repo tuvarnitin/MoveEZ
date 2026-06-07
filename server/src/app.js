6eb7bb27385b8c9db8d4a4b39f768cc4ad35d33a
Nitin Tuvar <nitintuvar2003@gmail.com>
2026-06-07 19:37:52 +0530

    Allow all origins for CORS

--- server/src/app.js
+++ server/src/app.js
@@ -11,7 +11,7 @@ import { sendOtp } from "./services/mail.service.js"
 
 const app = express()
 app.use(cors({
-    origin: ["https://moveezzz.vercel.app", "http://localhost:5173"],
+    origin: true,
     credentials: true
 }));
 
