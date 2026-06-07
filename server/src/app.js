e9513a1a126976e776654550b2adeb577df3e1cc
tuvarnitin <nitintuvar2003@gmail.com>
2026-06-07 19:34:19 +0530

    .

--- server/src/app.js
+++ server/src/app.js
@@ -10,18 +10,16 @@ import authRoute from "./auth/auth.route.js"
 import { sendOtp } from "./services/mail.service.js"
 
 const app = express()
+app.use(cors({
+    origin: ["https://moveezzz.vercel.app", "http://localhost:5173"],
+    credentials: true
+}));
 
 dotenv.config()
 
 //Middlewares
 app.use(express.json())
 app.use(cookieParser())
-
-
-app.use(cors({
-    origin: ["https://moveezzz.vercel.app", "http://localhost:5173"],
-    credentials: true
-}));
 
 app.use('/api/auth/google', googleRouter);
 app.use("/api/auth",authRoute)
