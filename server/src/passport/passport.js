43cbd45e4ee2093c1de0e3d268eeaa22b6d4c56c
tuvarnitin <nitintuvar2003@gmail.com>
2026-06-08 00:13:31 +0530

    Added callback url

--- server/src/passport/passport.js
+++ server/src/passport/passport.js
@@ -9,7 +9,7 @@ dotenv.config()
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/api/auth/google/callback"
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
 },
     async function (accessToken, refreshToken, profile, cb) {
         try {
@@ -21,6 +21,8 @@ passport.use(new GoogleStrategy({
                     avatar: profile.photos[0].value,
                     emailVerified: true,
                     authProvider: "google"
+                },{
+                    new:true
                 });
                 
             if (!user) {
