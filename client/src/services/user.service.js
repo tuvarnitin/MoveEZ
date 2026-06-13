d5fb560ae896026d1caa384c5125eca2517e69fb
tuvarnitin <nitintuvar2003@gmail.com>
2026-06-13 17:02:20 +0530

    Added one more service for handling user's bank detail API

--- client/src/services/user.service.js
+++ client/src/services/user.service.js
@@ -1,9 +1,10 @@
 import apiClient from "../api/API";
 
 export const userService = {
-    uploadDocs:(data) => apiClient.post("/api/user/docs/upload",data,{
-        headers:{
-            "Content-Type":"multipart/form-data"
+    uploadDocs: (data) => apiClient.post("/api/user/docs/upload", data, {
+        headers: {
+            "Content-Type": "multipart/form-data"
         }
-    })
+    }),
+    hadnleUserBank: (data) => apiClient.post("/api/user/bank/details", data)
 }
