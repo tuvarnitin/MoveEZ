e2c44eed3e6a765a42317b06446da124bb2912e1
tuvarnitin <nitintuvar2003@gmail.com>
2026-06-13 17:16:12 +0530

    Wrapped with react memo

--- client/src/components/DocsFileInput.jsx
+++ client/src/components/DocsFileInput.jsx
@@ -5,12 +5,12 @@ import { motion } from "motion/react"
 import { MdOutlineCheckCircleOutline } from "react-icons/md";
 import { RiLoader2Line, RiUploadCloud2Line } from 'react-icons/ri'
 
-const DocsFileInput = React.memo(({ fields:{ title, subTitle, onChange, name, value}, errors, isLoading}) => {
+const DocsFileInput = React.memo(({ name, title, file, error, onChange, subTitle, isLoading }) => {
     return (
         <div>
             <motion.label
                 whileHover={{ scale: 1.02 }}
-                className={`flex items-center justify-between p-4 rounded-xl border cursor-pointer hover:border-background  transition ${value && "border-green-500 hover:border-green-500"} ${errors[name] && "border-red-500 hover:border-red-500"}`}
+                className={`flex items-center justify-between p-4 rounded-xl border cursor-pointer hover:border-background  transition ${file && "border-green-500 hover:border-green-500"} ${error && "border-red-500 hover:border-red-500"}`}
             >
                 <div>
                     <h1 className='font-bold sm:font-semibold text-start'>{title}</h1>
@@ -20,7 +20,7 @@ const DocsFileInput = React.memo(({ fields:{ title, subTitle, onChange, name, va
                     isLoading ?
                         <RiLoader2Line className='animate-spin' size={26} />
                         : (
-                            value ?
+                            file ?
                                 <MdOutlineCheckCircleOutline size={33} className='bg-green-500 text-white rounded-full' />
                                 :
                                 <div className='w-10 h-10 bg-background rounded-full text-white flex items-center justify-center'>
@@ -31,8 +31,8 @@ const DocsFileInput = React.memo(({ fields:{ title, subTitle, onChange, name, va
                 <input onChange={onChange} name={name} type="file" accept='image/*,.pdf' hidden maxLength={1} />
             </motion.label>
             {
-                errors[name] &&
-                <p className='text-xs text-left ml-2 mt-0.5 text-red-500'>{errors[name]}</p>
+                error &&
+                <p className='text-xs text-left ml-2 mt-0.5 text-red-500'>{error}</p>
             }
         </div>
     )
