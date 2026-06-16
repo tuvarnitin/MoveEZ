9c2c85effb954d7c3a1ec2c9c0ab2b3472622f53
tuvarnitin <nitintuvar2003@gmail.com>
2026-06-16 10:57:20 +0530

    Showing home page according to user role

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,14 +3,33 @@ import Hero from '../components/Hero'
 import VehicleCategories from '../components/VehicleCategories'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
+import { useSelector } from 'react-redux'
+import AdminDashboard from './AdminDashboard'
+import PartnerDashboard from './PartnerDashboard'
 
-const Home = ({setIsSidebarOpen}) => {
+const Home = ({ setIsSidebarOpen }) => {
+
+  const role = useSelector(state => state.user?.data?.role)
+
   return (
     <>
       <Navbar setIsSidebarOpen={setIsSidebarOpen} />
-    <Hero />
-    <VehicleCategories />
-    <Footer />
+      {
+        role && role === "admin"
+          ?
+          <AdminDashboard />
+          : (
+            role === "partner"
+              ?
+              <PartnerDashboard />
+              :
+              <>
+                <Hero />
+                <VehicleCategories />
+              </>
+          )
+      }
+      <Footer />
     </>
   )
 }
