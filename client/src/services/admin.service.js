f48f5e34ca2d5058a4ce35af9753bb85d63006aa
tuvarnitin <nitintuvar2003@gmail.com>
2026-06-19 17:47:17 +0530

    Added two more admin services

--- client/src/services/admin.service.js
+++ client/src/services/admin.service.js
@@ -5,4 +5,6 @@ export const adminService = {
     fetchPartnerData: (data) => apiClient.get(`/api/admin/reviews/partner/${data.id}`),
     approvePartner: (data) => apiClient.get(`/api/admin/reviews/partner/approve/${data.id}`),
     rejectPartner: (data) => apiClient.post(`/api/admin/reviews/partner/reject/${data.id}`,data),
+    startVideoKyc: (data) => apiClient.get(`/api/admin/video-kyc/start/${data.id}`,data),
+    videoKycComplete: (data) => apiClient.post(`/api/admin/video-kyc/complete`,data)
 }
