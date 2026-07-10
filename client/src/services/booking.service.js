9860a2a64a3654c1c7cb0a8c8e5599da08dd6b16
tuvarnitin <nitintuvar2003@gmail.com>
2026-07-10 18:23:44 +0530

    Created booking service

---
+++ client/src/services/booking.service.js
@@ -0,0 +1,7 @@
+import apiClient from "../api/API";
+
+export const bookingService = {
+    requestBooking: (data) => apiClient.post(`/api/booking`, data),
+    fetchActiveBooking: (data) => apiClient.get(`/api/booking/active`, data),
+    cancelBooking: (data) => apiClient.get(`/api/booking/cancle/${data.id}`, data)
+}
