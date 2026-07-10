363245eaf6ef8fde67c4f3bd409e60fda9d04178
tuvarnitin <nitintuvar2003@gmail.com>
2026-07-10 18:21:19 +0530

    Created partner's endpoint for booking handlings

--- server/src/partner/partner.contoller.js
+++ server/src/partner/partner.contoller.js
@@ -1,3 +1,5 @@
+import bookingModel from "../booking/booking.model.js"
+
 export const requestKyc = async (req, res) => {
     try {
         const partner = req.user
@@ -25,4 +27,103 @@ export const requestKyc = async (req, res) => {
             error
         })
     }
+}
+
+export const fetchPendingRequestCount = async (req, res) => {
+    try {
+        const user = req.user
+        const pendingRequestsCount = await bookingModel.countDocuments({
+            driver: user._id,
+            bookingStatus: "requested"
+        })
+        return res.status(200).json({
+            success: true,
+            pendingRequestsCount
+        })
+    } catch (error) {
+        return res.status(200).json({
+            success: false,
+            pendingRequestsCount: 0,
+            message: "Internal server error (Fetch Pending Request Count)",
+            error
+        })
+    }
+}
+
+export const fetchPendingBookingRequests = async (req, res) => {
+    try {
+        const user = req.user
+        const bookings = await bookingModel.find({
+            driver: user._id,
+            bookingStatus: "requested"
+        })
+        return res.status(200).json({
+            success: true,
+            bookings
+        })
+    } catch (error) {
+        return res.status(200).json({
+            success: false,
+            bookings: 0,
+            message: "Internal server error (Fetch Pending Request Count)",
+            error
+        })
+    }
+}
+
+export const acceptBooking = async (req, res) => {
+   try {
+       const id = req.params.id
+       const booking = await bookingModel.findById(id);
+       if (!booking || booking.bookingStatus !== "requested") {
+           return res.status(400).json({
+               message: "Invalid request",
+               success: false
+           })
+       }
+
+       booking.bookingStatus = "awaiting_payment"
+       booking.paymentDeadline = new Date(Date.now() + 5 * 60 * 1000)
+       await booking.save()
+
+       return res.status(200).json({
+           message: "Booking accepted",
+           booking,
+           success: true
+       })
+   } catch (error) {
+       return res.status(500).json({
+           message: "Internal server error (Accept Booking)",
+           success: false,
+           error
+       })
+   }
+}
+
+export const rejectBooking = async (req, res) => {
+   try {
+       const id = req.params.id
+       const booking = await bookingModel.findById(id);
+       if (!booking || booking.bookingStatus !== "requested") {
+           return res.status(400).json({
+               message: "Invalid request",
+               success: false
+           })
+       }
+
+       booking.bookingStatus = "rejected"
+       await booking.save()
+
+       return res.status(200).json({
+           message: "Booking rejected",
+           booking,
+           success: true
+       })
+   } catch (error) {
+       return res.status(500).json({
+           message: "Internal server error (Reject Booking)",
+           success: false,
+           error
+       })
+   }
 }
--- server/src/partner/partner.route.js
+++ server/src/partner/partner.route.js
@@ -1,8 +1,12 @@
 import { Router } from "express"
-import { requestKyc } from "./partner.contoller.js"
+import { acceptBooking, fetchPendingBookingRequests, fetchPendingRequestCount, rejectBooking, requestKyc } from "./partner.contoller.js"
 
 const partnerRouter = Router();
 
 partnerRouter.get("/request/video-kyc", requestKyc)
+partnerRouter.get("/pending-request-count", fetchPendingRequestCount)
+partnerRouter.get("/pending-booking-requests", fetchPendingBookingRequests)
+partnerRouter.get("/accept-booking/:id", acceptBooking)
+partnerRouter.get("/reject-booking/:id", rejectBooking)
 
 export default partnerRouter
