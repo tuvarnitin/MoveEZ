2463133b7883a6473f7d8464c873c76b00f631b6
tuvarnitin <nitintuvar2003@gmail.com>
2026-07-13 16:36:40 +0530

    Endoint for fetching active booking

--- server/src/partner/partner.route.js
+++ server/src/partner/partner.route.js
@@ -1,11 +1,12 @@
 import { Router } from "express"
-import { acceptBooking, fetchPendingBookingRequests, fetchPendingRequestCount, rejectBooking, requestKyc } from "./partner.contoller.js"
+import { acceptBooking, fetchActiveBookings, fetchPendingBookingRequests, fetchPendingRequestCount, rejectBooking, requestKyc } from "./partner.contoller.js"
 
 const partnerRouter = Router();
 
 partnerRouter.get("/request/video-kyc", requestKyc)
 partnerRouter.get("/pending-request-count", fetchPendingRequestCount)
 partnerRouter.get("/pending-booking-requests", fetchPendingBookingRequests)
+partnerRouter.get("/booking/active", fetchActiveBookings)
 partnerRouter.get("/accept-booking/:id", acceptBooking)
 partnerRouter.get("/reject-booking/:id", rejectBooking)
 
